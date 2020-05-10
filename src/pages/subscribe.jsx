e3a5d5b1cadc0100ed37d4c8fa5d48c0718ee891
selkasse <selkasse@gmu.edu>
2020-05-10 12:00:57 -0400

    modify subscribe page

--- src/pages/subscribe.jsx
+++ src/pages/subscribe.jsx
@@ -150,34 +150,7 @@ class SubscribePage extends Component {
                                     </div>
                                 </MainHeader>
                                 <div className="post">
-                                    <h2>Cutting the Ribbon</h2>
-                                    <p>To celebrate the launch of <i>The Engine</i>, I'm doing a giveaway!</p>
 
-                                    <p>
-                                        Picking up software development can be daunting. Sometimes, a seasoned professional's explanation is much more helpful than reading documentation. It can be hard to know where to look, though. Throughout my learning process, I have come across some content creators that have allowed me to understand concepts much more quickly (and at a much deeper level), when compared to combing through docs. I want to share these resources with you.</p>
-
-                                    <h2>How does it work?</h2>
-
-                                    <p>
-                                        You attain eligibility for the giveaway by subscribing to the mailing list.  Five people will be chosen for the giveaway. These five people will be chosen at random. Entry will be open throughout the month of April.
-                                    </p>
-                                    <p>
-                                        The first three entrants chosen will each receive <strong>$100 worth of development courses</strong>.
-                                    </p>
-                                    <p>
-                                        The fourth and fifth entrants chosen will each receive <strong>$50 worth of development courses</strong>.
-                                    </p>
-                                    <h2>Which courses can I take?</h2>
-                                    <p>I'm giving away courses from content creators that I have personally taken courses from. As of right now, courses will be available from:</p>
-                                    <ul>
-                                        <li><a href="https://www.leveluptutorials.com/">Level Up Tutorials</a></li>
-                                        <li><a href="https://nickjanetakis.com/courses/">Nick Janetakis</a></li>
-                                        <li><a href="https://www.traversymedia.com/">Traversy Media</a></li>
-                                        <li><a href="https://wesbos.com/courses/">Wes Bos</a></li>
-                                    </ul>
-                                    <p>
-                                        If there's another creator that you'd like to see here, send an email to <a href="mailto:whistle@theengine.tech">whistle@theengine.tech</a>.
-                                    </p>
                                     <h2>no spam, fam</h2>
                                     <p>
                                        I'm not going to send you an email each time I put up a new post. I'm more likely to send out an email if I start working on a new 'series' of posts. I tend to work slow, so if between 1-3 emails a month sounds reasonable to you, cozy on up to that subscribe button. 
@@ -185,7 +158,7 @@ class SubscribePage extends Component {
                                     
                                     <EmailListForm /> <br />
                                     <p>
-                                        Thanks for clicking through. Stay safe.
+                                        Thanks for visiting!
                                     </p>
                                     <p>
                                         - Sharif
